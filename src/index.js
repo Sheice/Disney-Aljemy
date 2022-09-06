7e78e8d636b0c4b484922cbcc030e29c16685cea
Cejas Jaziel <jaziel.ismael.cejas@gmail.com>
2022-09-06 15:49:02 -0500

    DataBase connected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import app from "./app.js";
+import { sequelize } from "./db/db.js";
 
 
-app.listen(app.get('PORT'));
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    app.listen(app.get('PORT'));
 
-console.log(`Server listening on port ${app.get('PORT')}`);
+    console.log(`Server listening on port ${app.get('PORT')}`)
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
