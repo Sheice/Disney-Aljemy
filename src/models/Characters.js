9d6be7ab21b8c126bee60cccbddb22c3c0dcfae3
Cejas Jaziel <jaziel.ismael.cejas@gmail.com>
2022-09-07 11:17:37 -0500

    model of character done

---
+++ src/models/Characters.js
@@ -0,0 +1,26 @@
+import {sequelize} from '../db/db.js';
+import {DataTypes} from 'sequelize';
+
+
+export const Character = new sequelize.define('Characters', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    image: {
+        type: DataTypes.STRING
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    age: {
+        type: DataTypes.INTEGER
+    },
+    weight: {
+        type: DataTypes.FLOAT
+    },
+    history: {
+        type: DataTypes.STRING
+    }
+});
